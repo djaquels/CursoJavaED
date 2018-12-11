94d9e1a1e7c8515deb2524a749f8a33c3313eab3
heptorsj <hjacales@gmail.com>
2018-12-11 17:52:33 -0600

    Info

---
+++ OOP1/Ejecutar.java
@@ -0,0 +1,6 @@
+class Ejecutar{
+    public static void main(String[] args){
+        Persona persona = new Persona();
+        persona.nombre = "Hirving Lozano";
+    }
+}
---
+++ OOP1/Persona.java
@@ -0,0 +1,23 @@
+class Persona{
+    // Atributos
+    String nombre;
+    byte edad;
+    float peso;
+    float estatura;
+    // MÉTODOS
+    // Comportamiento
+    // 1.- Modificador de acceso 2.- Tipo De Retorno 3.- Nombre
+    // Constructor: 1.- Modificador de acceso 2.- Nombre de clase
+    // 3.- Parametros
+    public Persona(){
+
+    }
+    void hablar(){
+        System.out.println("Hola Amigo");
+
+    }
+    void comer(){
+        System.out.println("Estoy Comiendo");
+    }
+
+}
